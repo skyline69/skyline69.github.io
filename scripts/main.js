f106066356eb10c1bcc877a01c2731b28a66b124
skyline <67526259+skyline69@users.noreply.github.com>
2022-10-04 18:21:31 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
 const vid = document.getElementById("bg-video");
 
 
-if (document.readyState === "complete") {
-    vid.play();
-}
-
 vid.addEventListener("timeupdate", function () {
     if(this.currentTime >= (this.duration-5)) {
         this.currentTime = 3.0;
+    }
+});
+
+window.addEventListener('load', async () => {
+
+    try {
+        await vid.play();
+    } catch (err) {
+        vid.controls = true;
     }
 });
