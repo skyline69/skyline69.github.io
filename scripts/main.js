0da1aa63128b19df5d77bc4b2bf62f6f318dcee2
wunderkekshd <wunderkekshd@protonmail.com>
2022-10-05 13:56:31 +0200

    background video deleted and created new custom background that is responsive, because the page had to load too much.

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,8 @@
-const vid = document.getElementById("bg-video");
 const homebtn = document.getElementById("home-btn");
 const projectsbtn = document.getElementById("projects-btn");
 const aboutbtn = document.getElementById("about-btn");
 const contactbtn = document.getElementById("contact-btn");
 
-vid.addEventListener("timeupdate", function () {
-    if(this.currentTime >= (this.duration-5)) {
-        this.currentTime = 3.0;
-    }
-});
-
-window.addEventListener('load', async () => {
-
-    try {
-        await vid.play();
-    } catch (err) {
-        vid.controls = true;
-    }
-});
 
 homebtn.addEventListener("click", () => {
     window.location = "/";
